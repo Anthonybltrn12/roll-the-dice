9ab3413498cc95b5f79850de418820f3bdb423dc
antbeltran <anthonybltrn@gmail.com>
2026-04-17 14:18:47 -0500

    added comments

--- src/main/java/com/pluralsight/RollingDice.java
+++ src/main/java/com/pluralsight/RollingDice.java
@@ -4,20 +4,21 @@ public class RollingDice {
 
     public static void main(String[] args) {
         DiceRoll dice = new DiceRoll();
-
+//initalize variables
         int roll1;
         int roll2;
         int r2 = 0;
         int r4 = 0;
         int r6 = 0;
         int r7 = 0;
-
+//for loop to actually roll the dice
         for(int i = 0;  i <=100; i++){
             roll1 = dice.Roll();
             roll2 = dice.Roll();
             int sum = roll1 + roll2;
 
-            System.out.println("Roll 1:" + roll1 + "-" + roll2 + " Sum: " + sum);
+            System.out.println("Roll " + i + ": " + roll1 + "-" + roll2 + " Sum: " + sum);
+            //adding the rolls to the respective variables
             switch(sum){
                 case 2:
                     r2 += 1;
@@ -30,7 +31,7 @@ public class RollingDice {
             }
 
             }
-        System.out.println("# of sum 2: " + r2 + "\n # of sum 4: " + r4 + "\n # of sum 6: " + r6 + "\n # of sum 7: " + r7);
+        System.out.println(" # of sum 2: " + r2 + "\n # of sum 4: " + r4 + "\n # of sum 6: " + r6 + "\n # of sum 7: " + r7);
         }
 
 
